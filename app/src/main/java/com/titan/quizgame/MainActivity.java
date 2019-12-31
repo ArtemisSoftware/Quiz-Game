8ab9ee1ee588e77a1cf3715be506a042df21e169
gustavo <gustavo.maia@b2b.com.pt>
2019-12-31 11:16:50 +0000

    Toolbar on main activity

--- app/src/main/java/com/titan/quizgame/MainActivity.java
+++ app/src/main/java/com/titan/quizgame/MainActivity.java
@@ -1,8 +1,14 @@
 package com.titan.quizgame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.titan.quizgame.settings.SettingsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            // launch settings activity
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
