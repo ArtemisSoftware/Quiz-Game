7681f0ca79f7edfd2e61f77b1fd7233a3a2eb284
gustavo <gustavo.maia@b2b.com.pt>
2019-12-31 14:05:15 +0000

    Splash logic

--- app/src/main/java/com/titan/quizgame/SplashActivity.java
+++ app/src/main/java/com/titan/quizgame/SplashActivity.java
@@ -3,6 +3,7 @@ package com.titan.quizgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -15,14 +16,35 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        handler=new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },3000);
+        SharedPreferences settings = getSharedPreferences(getPackageName(),0);
+
+        boolean firstRun = settings.getBoolean("firstRun",true);
+
+        if(firstRun == true){//if running for first time Splash will load for first time
+
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean("firstRun", false);
+            editor.commit();
+
+
+            handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    initMainActivity();
+                }
+            },2000);
+        }
+        else{
+            initMainActivity();
+        }
+    }
+
+
+    private void initMainActivity(){
+        Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
