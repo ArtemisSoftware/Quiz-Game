306f081e80a54b822a39b07e32adae4e27acb027
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-01-01 18:18:22 +0000

    QuestionDao

---
+++ app/src/main/java/com/titan/quizgame/quiz/persistence/QuestionDao.java
@@ -0,0 +1,35 @@
+package com.titan.quizgame.quiz.persistence;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.titan.quizgame.quiz.models.Question;
+
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Maybe;
+import io.reactivex.Single;
+
+@Dao
+public interface QuestionDao {
+
+    @Query("SELECT * from questions")
+    public Flowable<Question> getQuestions(int id);
+
+    // Emits the number of users added to the database.
+    @Insert
+    public Maybe<Integer> insertQuestions(List<Question> questions);
+
+    // Makes sure that the operation finishes successfully.
+    @Insert
+    public Completable insertQuestions(Question... questions);
+
+    /* Emits the number of users removed from the database. Always emits at
+       least one user. */
+    @Delete
+    public Single<Integer> deleteQuestions(List<Question> users);
+}
