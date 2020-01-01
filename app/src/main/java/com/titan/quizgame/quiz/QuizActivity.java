4727d336d6c87857139a664da3b6f4a80f9e8560
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-01-01 21:13:41 +0000

    Quiz Activity

--- app/src/main/java/com/titan/quizgame/quiz/QuizActivity.java
+++ app/src/main/java/com/titan/quizgame/quiz/QuizActivity.java
@@ -2,7 +2,15 @@ package com.titan.quizgame.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.titan.quizgame.R;
 import com.titan.quizgame.quiz.models.Question;
@@ -10,13 +18,54 @@ import com.titan.quizgame.quiz.persistence.QuestionDao;
 import com.titan.quizgame.quiz.persistence.QuestionDatabase;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.reactivex.functions.Consumer;
 
 public class QuizActivity extends AppCompatActivity {
 
+    @BindView(R.id.text_view_question)
+    TextView textViewQuestion;
+
+    @BindView(R.id.text_view_score)
+    TextView textViewScore;
+
+    @BindView(R.id.text_view_question_count)
+    TextView textViewQuestionCount;
+
+    @BindView(R.id.text_view_countdown)
+    TextView textViewCountDown;
+
+    @BindView(R.id.radio_group)
+    RadioGroup rbGroup;
+
+    @BindView(R.id.radio_button1)
+    RadioButton rb1;
+
+    @BindView(R.id.radio_button2)
+    RadioButton rb2;
+
+    @BindView(R.id.radio_button3)
+    RadioButton rb3;
+
+    @BindView(R.id.button_confirm_next)
+    Button buttonConfirmNext;
+
     private QuestionDao questionDao;
+
+    private ColorStateList textColorDefaultRb;
+
+    private List<Question> questionList;
+    private int questionCounter;
+    private int questionCountTotal;
+    private Question currentQuestion;
+
+    private int score;
+    private boolean answered;
 
 
     @Override
@@ -24,10 +73,85 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        ButterKnife.bind(this);
+
         questionDao = QuestionDatabase.getInstance(this).questionDao();
+
+        textColorDefaultRb = rb1.getTextColors();
+
 
         getQuestions();
     }
+
+    private void showNextQuestion() {
+        rb1.setTextColor(textColorDefaultRb);
+        rb2.setTextColor(textColorDefaultRb);
+        rb3.setTextColor(textColorDefaultRb);
+        rbGroup.clearCheck();
+
+        if (questionCounter < questionCountTotal) {
+            currentQuestion = questionList.get(questionCounter);
+
+            textViewQuestion.setText(currentQuestion.getQuestion());
+            rb1.setText(currentQuestion.getOption1());
+            rb2.setText(currentQuestion.getOption2());
+            rb3.setText(currentQuestion.getOption3());
+
+            questionCounter++;
+            textViewQuestionCount.setText("Question: " + questionCounter + "/" + questionCountTotal);
+            answered = false;
+            buttonConfirmNext.setText("Confirm");
+        } else {
+            finishQuiz();
+        }
+    }
+
+
+    private void checkAnswer() {
+        answered = true;
+
+        RadioButton rbSelected = findViewById(rbGroup.getCheckedRadioButtonId());
+        int answerNr = rbGroup.indexOfChild(rbSelected) + 1;
+
+        if (answerNr == currentQuestion.getAnswerNr()) {
+            score++;
+            textViewScore.setText("Score: " + score);
+        }
+
+        showSolution();
+    }
+
+    private void showSolution() {
+        rb1.setTextColor(Color.RED);
+        rb2.setTextColor(Color.RED);
+        rb3.setTextColor(Color.RED);
+
+        switch (currentQuestion.getAnswerNr()) {
+            case 1:
+                rb1.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 1 is correct");
+                break;
+            case 2:
+                rb2.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 2 is correct");
+                break;
+            case 3:
+                rb3.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 3 is correct");
+                break;
+        }
+
+        if (questionCounter < questionCountTotal) {
+            buttonConfirmNext.setText("Next");
+        } else {
+            buttonConfirmNext.setText("Finish");
+        }
+    }
+
+    private void finishQuiz() {
+        finish();
+    }
+
 
 
     private void getQuestions(){
@@ -38,6 +162,10 @@ public class QuizActivity extends AppCompatActivity {
                     @Override
                     public void accept(List<Question> questions) throws Exception {
 
+                        questionList = questions;
+                        questionCountTotal = questionList.size();
+                        Collections.shuffle(questionList);
+                        showNextQuestion();
                     }
                 },
                 new Consumer<Throwable>() {
@@ -48,6 +176,19 @@ public class QuizActivity extends AppCompatActivity {
                 }
         );
 
+    }
+
+    @OnClick(R.id.button_confirm_next)
+    public void onButtonClick(View view) {
+        if (!answered) {
+            if (rb1.isChecked() || rb2.isChecked() || rb3.isChecked()) {
+                checkAnswer();
+            } else {
+                Toast.makeText(QuizActivity.this, "Please select an answer", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            showNextQuestion();
+        }
     }
 
 }
