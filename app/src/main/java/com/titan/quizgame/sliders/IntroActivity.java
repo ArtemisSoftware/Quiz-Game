56353c3477c4085452bff7e0c2de29b36b8b4c28
gustavo <gustavo.maia@b2b.com.pt>
2020-01-02 13:55:01 +0000

    Intro slider layout

---
+++ app/src/main/java/com/titan/quizgame/sliders/IntroActivity.java
@@ -0,0 +1,16 @@
+package com.titan.quizgame.sliders;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.titan.quizgame.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
