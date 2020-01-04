d1164f10fb7803614e2b96679a594debad421fa7
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-01-04 20:03:21 +0000

    Save Highscore to preferences

--- app/src/main/java/com/titan/quizgame/MainActivity.java
+++ app/src/main/java/com/titan/quizgame/MainActivity.java
@@ -1,16 +1,20 @@
 package com.titan.quizgame;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.titan.quizgame.quiz.ActivityCode;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
@@ -32,6 +36,13 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String KEY_HIGHSCORE = "keyHighscore";
+    private int highscore;
+
+    @BindView(R.id.text_view_highscore)
+    TextView textViewHighscore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,11 +53,38 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        loadHighscore();
+
+    }
+
+
+
+
+    private void startQuiz() {
+        Intent intent = new Intent(this, QuizActivity.class);
+        startActivityForResult(intent, ActivityCode.REQUEST_CODE_QUIZ);
         //Permissions.requestAppPermission(this);
 
         initIntro();
 
     }
+
+    private void loadHighscore() {
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        highscore = prefs.getInt(KEY_HIGHSCORE, 0);
+        textViewHighscore.setText("Highscore: " + highscore);
+    }
+
+    private void updateHighscore(int highscoreNew) {
+        highscore = highscoreNew;
+        textViewHighscore.setText("Highscore: " + highscore);
+
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(KEY_HIGHSCORE, highscore);
+        editor.apply();
+    }
+
 
 
     @OnClick(R.id.button_start_quiz)
@@ -55,9 +93,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void startQuiz() {
-        Intent intent = new Intent(this, QuizActivity.class);
-        startActivity(intent);
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ActivityCode.REQUEST_CODE_QUIZ) {
+            if (resultCode == RESULT_OK) {
+                int score = data.getIntExtra(ActivityCode.EXTRA_SCORE, 0);
+                if (score > highscore) {
+                    updateHighscore(score);
+                }
+            }
+        }
     }
 
 
