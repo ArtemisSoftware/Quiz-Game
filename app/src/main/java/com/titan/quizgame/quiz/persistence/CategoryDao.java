8b5120285d9b542e00d5b0ee1f6e24907ff11724
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-01-07 18:51:59 +0000

    Categories queries

--- app/src/main/java/com/titan/quizgame/quiz/persistence/CategoryDao.java
+++ app/src/main/java/com/titan/quizgame/quiz/persistence/CategoryDao.java
@@ -2,12 +2,14 @@ package com.titan.quizgame.quiz.persistence;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
 
 import com.titan.quizgame.quiz.models.Category;
 import com.titan.quizgame.quiz.models.Question;
 
 import java.util.List;
 
+import io.reactivex.Flowable;
 import io.reactivex.Maybe;
 
 @Dao
@@ -15,5 +17,8 @@ public interface CategoryDao {
 
     // Emits the number of users added to the database.
     @Insert
-    public Maybe<long[]> insertCategories(List<Category> categories);
+    Maybe<long[]> insertCategories(List<Category> categories);
+
+    @Query("SELECT * from categories ")
+    Flowable<List<Question>> getCategories();
 }
