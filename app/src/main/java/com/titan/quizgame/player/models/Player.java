ced50e3b2ec4a90dc9adb6ba489523370a623e88
gustavo <gustavo.maia@b2b.com.pt>
2020-01-09 16:45:00 +0000

    Player model

---
+++ app/src/main/java/com/titan/quizgame/player/models/Player.java
@@ -0,0 +1,11 @@
+package com.titan.quizgame.player.models;
+
+public class Player {
+
+    private int id;
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+}
