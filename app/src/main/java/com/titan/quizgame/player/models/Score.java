4ce06627b9d1ca2d48edc43be8237da30544c5bb
gustavo <gustavo.maia@b2b.com.pt>
2020-01-09 17:04:44 +0000

    Score model

---
+++ app/src/main/java/com/titan/quizgame/player/models/Score.java
@@ -0,0 +1,13 @@
+package com.titan.quizgame.player.models;
+
+public class Score {
+
+    private int id;
+    private int points;
+    private int categoryId;
+    private String difficulty;
+    private int playerId;
+
+
+
+}
