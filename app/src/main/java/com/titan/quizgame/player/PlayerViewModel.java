ec311dfa6358f6e2a887dc88c30dff13bdcfb510
gustavo <gustavo.maia@b2b.com.pt>
2020-01-22 13:50:01 +0000

    Method to load leaderboard and save score and players int the player activities

--- app/src/main/java/com/titan/quizgame/player/PlayerViewModel.java
+++ app/src/main/java/com/titan/quizgame/player/PlayerViewModel.java
@@ -3,10 +3,24 @@ package com.titan.quizgame.player;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.titan.quizgame.player.models.Board;
+import com.titan.quizgame.player.models.Player;
+import com.titan.quizgame.player.models.Score;
 import com.titan.quizgame.player.repository.PlayerRepository;
+import com.titan.quizgame.quiz.models.Question;
 import com.titan.quizgame.ui.Resource;
 
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.CompletableObserver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 import timber.log.Timber;
 
 public class PlayerViewModel extends ViewModel {
@@ -34,5 +48,100 @@ public class PlayerViewModel extends ViewModel {
     }
 
 
+
+
+    public void saveScore(Player player, Score score) {
+
+        playerRepository.playerExists(player.getName())
+                .map(new Function<Integer, Integer>() {
+                    @Override
+                    public Integer apply(Integer response) throws Exception {
+
+                        return response; // B.
+                    }
+                })
+                .flatMapCompletable(new Function<Integer, Completable>() {
+                    @Override
+                    public Completable apply(Integer response) throws Exception {
+
+                        Completable action;
+
+                        if (response == 0) { //nao existe
+                            action = Completable.concatArray(playerRepository.savePlayer(player), playerRepository.saveScore(score));
+                        }
+                        else {
+                            action = Completable.concatArray(playerRepository.saveScore(score));
+                        }
+
+                        return action;
+                    }
+                })
+                .doOnSubscribe(__ -> {
+                    //Log.w(LOG_TAG, "Begin transaction. " + Thread.currentThread().toString());
+                    //mRoomDatabase.beginTransaction();
+                })
+                .doOnComplete(() -> {
+                    //Log.w(LOG_TAG, "Set transaction successful."  + Thread.currentThread().toString());
+                    //mRoomDatabase.setTransactionSuccessful();
+                })
+                .doFinally(new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        Timber.d("End transaction." + Thread.currentThread().toString());
+                        //mRoomDatabase.endTransaction();
+                    }
+                })
+                .subscribeOn(Schedulers.single())
+                .observeOn(AndroidSchedulers.mainThread()) // ON UI THREAD
+                .subscribeWith(new CompletableObserver() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                        if (d != null) {
+                            disposables.add(d);
+                        }
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Timber.d("onComplete." + Thread.currentThread().toString());
+                        playersLiveData.setValue(Resource.success(null, "Score saved"));
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        //Log.e(LOG_TAG, "onError." + Thread.currentThread().toString());
+                    }
+                });
+
+    }
+
+
+
+    public void loadLeaderBoard() {
+
+        disposables.add(
+                //getting flowable to subscribe consumer that will access the data from Room database.
+                playerRepository.getLeaderBoard()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                new Consumer<List<Board>>() {
+                                    @Override
+                                    public void accept(List<Board> questions) throws Exception {
+
+                                        playersLiveData.setValue(Resource.success(questions, ""));
+
+                                    }
+                                },
+                                new Consumer<Throwable>() {
+                                    @Override
+                                    public void accept(Throwable throwable) throws Exception {
+
+                                    }
+                                }
+                        )
+        );
+    }
 
 }
--- app/src/main/java/com/titan/quizgame/player/repository/PlayerRepository.java
+++ app/src/main/java/com/titan/quizgame/player/repository/PlayerRepository.java
@@ -2,15 +2,21 @@ package com.titan.quizgame.player.repository;
 
 import androidx.annotation.NonNull;
 
+import com.titan.quizgame.player.models.Board;
 import com.titan.quizgame.player.models.Player;
 import com.titan.quizgame.player.models.Score;
 import com.titan.quizgame.player.persistence.PlayerDao;
 import com.titan.quizgame.player.persistence.ScoreDao;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.Single;
 
 @Singleton
 public class PlayerRepository {
@@ -39,9 +45,12 @@ public class PlayerRepository {
     }
 
 
-/*
-    public Flowable<List<Question>> getQuestions(String difficulty, int categoryId) {
-        return questionDao.getQuestions(difficulty, categoryId);
+    public Single<Integer> playerExists(String name) {
+        return playerDao.playerCount(name);
     }
-*/
+
+    public Observable<List<Board>> getLeaderBoard() {
+        return playerDao.getLeaderBoard();
+    }
+
 }
