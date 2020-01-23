63fba9f4402f46839b3a603f387b58ea10840977
gustavo <gustavo.maia@b2b.com.pt>
2020-01-23 17:13:25 +0000

    LeaderBoardActivity load data

--- app/src/main/java/com/titan/quizgame/player/LeaderBoardActivity.java
+++ app/src/main/java/com/titan/quizgame/player/LeaderBoardActivity.java
@@ -6,16 +6,23 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.bumptech.glide.util.ViewPreloadSizeProvider;
 import com.mikhaellopez.circularimageview.CircularImageView;
 import com.titan.quizgame.BaseActivity;
 import com.titan.quizgame.R;
 import com.titan.quizgame.player.adapters.BoardRecyclerAdapter;
+import com.titan.quizgame.player.models.Board;
+import com.titan.quizgame.quiz.models.Category;
 import com.titan.quizgame.ui.Resource;
+import com.titan.quizgame.util.constants.ActivityCode;
 import com.titan.quizgame.util.viewmodel.ViewModelProviderFactory;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -24,6 +31,18 @@ import butterknife.ButterKnife;
 import timber.log.Timber;
 
 public class LeaderBoardActivity extends BaseActivity {
+
+    @BindView(R.id.txt_name)
+    TextView txt_name;
+
+    @BindView(R.id.txt_difficulty)
+    TextView txt_difficulty;
+
+    @BindView(R.id.txt_score)
+    TextView txt_score;
+
+    @BindView(R.id.txt_category)
+    TextView txt_category;
 
 
     @BindView(R.id.board_list)
@@ -78,7 +97,10 @@ public class LeaderBoardActivity extends BaseActivity {
 
                     case SUCCESS:
 
-                        //set values
+                        fillTopPlayer(((List<Board>) resource.data).get(0));
+
+                        ((List<Board>) resource.data).remove(0);
+                        boardRecyclerAdapter.setResults((List<Board>) resource.data);
                         break;
 
                     case ERROR:
@@ -89,5 +111,17 @@ public class LeaderBoardActivity extends BaseActivity {
             }
         });
     }
+
+
+    private void fillTopPlayer(Board board){
+
+        txt_name.setText(board.getName());
+        txt_difficulty.setText(board.getDifficulty());
+        txt_score.setText(board.getPoints() + "");
+        txt_category.setText(board.getCategory());
+
+        txt_category.setText(board.getCategory());
+    }
+
 
 }
