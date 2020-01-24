6bfea0ef08f81c998f81908f9d1c9872ac757832
gustavo <gustavo.maia@b2b.com.pt>
2020-01-24 12:30:56 +0000

    Lottie dependencies

---
+++ app/src/main/java/com/titan/quizgame/util/UIMessages.java
@@ -0,0 +1,14 @@
+package com.titan.quizgame.util;
+
+import android.app.Activity;
+import android.content.Context;
+
+import com.labters.lottiealertdialoglibrary.DialogTypes;
+import com.labters.lottiealertdialoglibrary.LottieAlertDialog;
+
+public class UIMessages {
+
+
+
+
+}
