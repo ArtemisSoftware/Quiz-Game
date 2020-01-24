0410a513ca3c54553af14ccf03c6ae1013f8ac01
gustavo <gustavo.maia@b2b.com.pt>
2020-01-24 12:31:21 +0000

    UIMessages

--- app/src/main/java/com/titan/quizgame/util/UIMessages.java
+++ app/src/main/java/com/titan/quizgame/util/UIMessages.java
@@ -9,6 +9,16 @@ import com.labters.lottiealertdialoglibrary.LottieAlertDialog;
 public class UIMessages {
 
 
+    public static void success(Activity activity){
 
+        LottieAlertDialog alertDialog = new LottieAlertDialog.Builder(activity, DialogTypes.TYPE_SUCCESS)
+                .setTitle("Register")
+                .setDescription("Player and score saved")
+                .build();
+        alertDialog.setCancelable(true);
+        alertDialog.show();
+
+
+    }
 
 }
