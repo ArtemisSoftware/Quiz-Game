1e848265e8a1d4b41a6f3c1be3e5af6e304c5ce8
gustavo <gustavo.maia@b2b.com.pt>
2020-01-31 09:32:17 +0000

    NetworkModule

---
+++ app/src/main/java/com/titan/quizgame/di/NetworkModule.java
@@ -0,0 +1,71 @@
+package com.titan.quizgame.di;
+
+import com.titan.quizgame.util.constants.Api;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import timber.log.Timber;
+
+@Module
+public class NetworkModule {
+
+
+/*
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient() {
+
+        OkHttpClient client = new OkHttpClient.Builder()
+
+                //establish connection to server
+                .connectTimeout(ApiConstants.CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+
+                //time between each byte read from the server
+                .readTimeout(ApiConstants.READ_TIMEOUT, TimeUnit.SECONDS)
+
+                //time between each byte sent to server
+                .writeTimeout(ApiConstants.WRITE_TIMEOUT, TimeUnit.SECONDS)
+
+                .retryOnConnectionFailure(false)
+                .addInterceptor(new UrlInterceptor())
+                .build();
+
+        Timber.d("Providing OkHttpClient: " + client);
+        return client;
+    }
+*/
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(/*OkHttpClient okHttpClient*/) {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Api.IMGUR_BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                //.client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Timber.d("Providing retrofit: " + retrofit);
+        return retrofit;
+    }
+
+
+/*
+    @Provides
+    @Singleton
+    FlickrApi provideFlickrApiInterface(Retrofit retrofit) {
+
+        FlickrApi api = retrofit.create(FlickrApi.class);
+        Timber.d("Providing FlickrApi: " + api);
+
+        return api;
+    }
+    */
+
+}
